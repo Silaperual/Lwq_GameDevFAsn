fb0a9aebdf55a646b05c82241973e3dc36b03c7c
Shirley <1493008661@qq.com>
2020-10-13 19:35:00 +0800

    Third Assignment
    A improved ball collider demo

---
+++ Third Assignment/Assets/OtherSphere.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherSphere : MonoBehaviour
+{
+    public float friction;
+    public float mass;
+    public float radius;
+
+    [NonSerialized]
+    public Vector3 currentV;
+    public Vector3 curV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curV = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //摩擦力
+        Vector3 frictionDeltaV = -Time.deltaTime * friction * currentV.normalized;
+        //防止摩擦力反向运动
+        Vector3 finalV = currentV + frictionDeltaV;
+        if (finalV.x * currentV.x <= 0)
+            frictionDeltaV.x = -currentV.x;
+        if (finalV.y * currentV.y <= 0)
+            frictionDeltaV.y = -currentV.y;
+        if (finalV.z * currentV.z <= 0)
+            frictionDeltaV.z = -currentV.z;
+
+        //应用加速度
+        curV = currentV + frictionDeltaV;
+        transform.Translate((curV + currentV) * Time.deltaTime / 2);
+        currentV = curV;
+        currentV.y = 0;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "wall")
+        {
+            Vector3 v1 = currentV;
+            ContactPoint contactPoint = collision.contacts[0];
+            Vector3 newDir = curV;
+            newDir = Vector3.Reflect(currentV, contactPoint.normal);
+            currentV = newDir;
+            Debug.Log("大球碰到了");
+            //Quaternion rotation = Quaternion.FromToRotation(preV, newDir);
+
+        }
+        currentV.y = 0;
+    }
+}
---
+++ Third Assignment/Assets/Script/Wall.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public float friction;
+    public float mass;
+    public GameObject[] wall;
+    private Vector3 pos;
+    private Vector3 preV;
+    private Vector3 prePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //检测是否与墙碰撞
+        foreach (GameObject wallObj in wall)
+        {
+            if (wallObj != null)
+            {
+                Vector3 wallPos = wallObj.transform.position;
+
+                if (Vector3.Distance(pos, wallPos) < 1)
+                {
+                    Debug.Log("碰到墙了");
+                    Vector3 v1 = Vector3.Reflect(preV, wallPos);
+                    preV = v1;
+
+                    transform.position = prePos;
+                }
+            }
+        }
+    }
+}
